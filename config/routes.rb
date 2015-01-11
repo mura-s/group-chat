cfefc804c368e331d29bb1702e23a26add4c0963
mura-s <seiya.muramatsu1@gmail.com>
2015-01-11 15:52:01 +0900

    ユーザ認証を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :destroy]
+  devise_for :users
 
-  root to: 'users#index'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
