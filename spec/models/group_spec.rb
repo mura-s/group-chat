db3ab3b4131ddd46e71b27b0b079b685e9dc139f
mura-s <seiya.muramatsu1@gmail.com>
2015-01-24 22:35:21 +0900

    FactoryGirlの設定を修正

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Group, :type => :model do
   describe '#name' do
     it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(20) }
   end
 end
